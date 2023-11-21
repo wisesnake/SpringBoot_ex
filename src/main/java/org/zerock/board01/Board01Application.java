4336b6b3929e9878c98b0daa22bb2486e0738a9d
wisesnake <wisesnake0618@gmail.com>
2023-11-21 15:07:17 +0900

    init: 프로젝트 datasource 및 controller 테스트 완료

---
+++ src/main/java/org/zerock/board01/Board01Application.java
@@ -0,0 +1,14 @@
+package org.zerock.board01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Board01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board01Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/zerock/board01/Board01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.board01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
