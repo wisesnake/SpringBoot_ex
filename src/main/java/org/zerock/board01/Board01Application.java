87882c5d45236f4a8594baeb6f35e98db646473f
wisesnake <wisesnake0618@gmail.com>
2023-11-21 19:25:35 +0900

    feat: thymeleaf 연습.

--- src/main/java/org/zerock/board01/Board01Application.java
+++ src/main/java/org/zerock/board01/Board01Application.java
@@ -2,8 +2,10 @@ package org.zerock.board01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Board01Application {
 
